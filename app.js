145b73b56a23d588e92cff5006dbc5f693263c5d
brainy <khoshfekr2001@protonmail.com>
2022-11-21 05:14:48 +0330

    register function with passport completed!

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
-//googleStr
-//findOrCreate
+const findOrCreate = require('mongoose-find-or-create')
 
 const app = express()
 
@@ -17,9 +16,63 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-app.get("/", function (req,res) {
-    res.render("register.ejs")
-})
+app.use(session({
+    secret: process.env.SECRET_ID,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize(undefined))
+app.use(passport.session(undefined))
+
+mongoose.connect(process.env.MONGODB_URL)
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema)
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id)
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user)  {
+        done(err, user)
+    });
+});
+
+app.route("/")
+    .get(function (req, res) {
+        res.render("register");
+    })
+    .post(function (req, res) {
+        const password = req.body.password
+        const email = req.body.username
+        const repeatPassword = req.body.repeatPassword
+
+        if(password === repeatPassword){
+            User.register({username: email}, password, function(err, user){
+                if(err) {
+                    console.log(err)
+                    res.redirect("/");
+                } else {
+                    passport.authenticate("local")(req, res, function () {
+                        res.redirect("/accountPage")
+                    });
+                }
+            });
+        } else {
+            console.log("password is not same!")
+            res.redirect("/")
+        }
+    });
 
 app.get("/login", function (req, res) {
     res.render("login.ejs")
